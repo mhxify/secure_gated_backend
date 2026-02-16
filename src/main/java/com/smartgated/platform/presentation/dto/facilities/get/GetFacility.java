7e4fc3ec7e204a85dfc6c4296798802f527d9988
mhxify <mohamedbenouarzeg1@gmail.com>
2026-02-16 08:46:25 +0100

    facility dto

---
+++ src/main/java/com/smartgated/platform/presentation/dto/facilities/get/GetFacility.java
@@ -0,0 +1,46 @@
+package com.smartgated.platform.presentation.dto.facilities.get;
+
+import java.util.UUID;
+
+public class GetFacility {
+
+    private UUID facilityId;
+
+    private String name ;
+
+    private Long capacity;
+
+    private String imageUrl;
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public Long getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(Long capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public UUID getFacilityId() {
+        return facilityId;
+    }
+
+    public void setFacilityId(UUID facilityId) {
+        this.facilityId = facilityId;
+    }
+}
