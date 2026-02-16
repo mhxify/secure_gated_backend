57fa1d467e902c772932892e1857cbd3a84f28e5
mhxify <mohamedbenouarzeg1@gmail.com>
2026-02-16 14:11:39 +0100

    fcm update dto

---
+++ src/main/java/com/smartgated/platform/presentation/dto/fcm/update/UpdateFcmToken.java
@@ -0,0 +1,14 @@
+package com.smartgated.platform.presentation.dto.fcm.update;
+
+public class UpdateFcmToken {
+    private String fcmToken;
+
+    public String getFcmToken() {
+        return fcmToken;
+    }
+
+    public void setFcmToken(String fcmToken) {
+        this.fcmToken = fcmToken;
+    }
+
+}
