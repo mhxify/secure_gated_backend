010dc3957578de0bbcca6f419be35e4b11de98ce
mhxify <mohamedbenouarzeg1@gmail.com>
2026-02-16 14:10:36 +0100

    security config package fixing

--- src/main/java/com/smartgated/platform/presentation/config/security/SecurityConfig.java
+++ src/main/java/com/smartgated/platform/presentation/config/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.smartgated.platform.presentation.config;
+package com.smartgated.platform.presentation.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
