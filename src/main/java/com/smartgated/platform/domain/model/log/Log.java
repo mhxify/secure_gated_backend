625292c51cd0de272d8858dfbaaaa406b1d222d8
Mohamed Ali Benouarzeg <mohamedbenouarzeg1@gmail.com>
2026-02-19 00:02:33 +0100

    log model

---
+++ src/main/java/com/smartgated/platform/domain/model/log/Log.java
@@ -0,0 +1,40 @@
+package com.smartgated.platform.domain.model.log;
+
+import com.smartgated.platform.domain.enums.user.UserRole;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "logs")
+public class Log {
+
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Id
+    private UUID logId;
+
+    private String personFullName ;
+
+    private UserRole userRole ;
+
+    private String logState ;
+
+    public Log(){}
+
+    public Log(
+            UUID logId ,
+            String personFullName ,
+            UserRole userRole ,
+            String logState
+    ) {
+        this.logId = logId ;
+        this.personFullName = personFullName ;
+        this.userRole = userRole ;
+        this.logState = logState ;
+    }
+
+}
